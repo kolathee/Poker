3ae4f05032584a4513b17d962e24e2ae99ba63d6
topfiiz <s6070037@kmitl.ac.th>
2013-10-04 22:20:47 +0700

    CHANGE: Double quote( " )  to Single quote ( ' ) in test case

--- poker_test.py
+++ poker_test.py
@@ -4,37 +4,37 @@ import poker
 class TestNumhand(unittest.TestCase):
 
     def test1_numhand(self):
-        """test_numhand ["2C","3C","4D","5H","6S"]-->[2,3,4,5,6]"""
-        actual = poker.numhand(["2C","3C","4D","5H","6S"])
+        """test_numhand ['2C','3C','4D','5H','6S']-->[2,3,4,5,6]"""
+        actual = poker.numhand(['2C','3C','4D','5H','6S'])
         expected = [2,3,4,5,6]
         self.assertEqual(actual, expected)
     def test2_numhand(self):
-        """test_numhand ["8D","9H","3D","3C","7S"]-->[8,9,3,3,7]"""
+        """test_numhand ['8D','9H','3D','3C','7S']-->[8,9,3,3,7]"""
         actual = poker.numhand(["8D","9H","3D","3C","7S"])
         expected = [8,9,3,3,7]
         self.assertEqual(actual, expected)
     def test3_numhand(self):
-        """test_numhand ["2D","2C","2H","6S","JD"]-->[2,2,2,6,J]"""
-        actual = poker.numhand(["2D","2C","2H","6S","JD"])
+        """test_numhand ['2D','2C','2H','6S','JD']-->[2,2,2,6,J]"""
+        actual = poker.numhand(['2D','2C','2H','6S','JD'])
         expected = [2,2,2,6,11]
         self.assertEqual(actual, expected)
 
 class TestDokhand(unittest.TestCase):
 
     def test1_dokhand(self):
-        """test_dokhand ["2C","3C","4D","5H","6S"]-->['C','C','D','H','S']"""
-        actual = poker.dokhand(["2C","3C","4D","5H","6S"])
+        """test_dokhand ['2C','3C','4D','5H','6S']-->['C','C','D','H','S']"""
+        actual = poker.dokhand(['2C','3C','4D','5H','6S'])
         expected = ['C','C','D','H','S']
         self.assertEqual(actual, expected)
     def test2_dokhand(self):
-        """test_dokhand ["JC","TC","9C","8C","7C"]-->['C','C','C','C','C']"""
-        actual = poker.dokhand(["JC","TC","9C","8C","7C"])
+        """test_dokhand ['JC','TC','9C','8C','7C']-->['C','C','C','C','C']"""
+        actual = poker.dokhand(['JC','TC','9C','8C','7C'])
         expected = ['C','C','C','C','C']
         self.assertEqual(actual, expected)
     def test3_dokhand(self):
-        """test_dokhand ["8D","9H","3D","3C","7S"]-->["D","H","D","C","S"]"""
-        actual = poker.dokhand(["8D","9H","3D","3C","7S"])
-        expected = ["D","H","D","C","S"]
+        """test_dokhand ['8D','9H','3D','3C','7S']-->['D','H','D','C','S']"""
+        actual = poker.dokhand(['8D','9H','3D','3C','7S'])
+        expected = ['D','H','D','C','S']
         self.assertEqual(actual, expected)
 
 class TestFlush(unittest.TestCase):

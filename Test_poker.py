c6a16787f3fc3af8ddffeea458ad6dbce5fd9897
topfiiz <s6070037@kmitl.ac.th>
2013-10-04 15:22:01 +0700

    Add dokhand

--- Test_poker.py
+++ Test_poker.py
@@ -8,6 +8,11 @@ class Testpoker(unittest.TestCase):
         actual = poker_test.numhand(["2C","3C","4D","5H","6S"])
         expected = [2,3,4,5,6]
         self.assertEqual(actual, expected)
+    def test1_dokhand(self):
+        """test_dokhand ["2C","3C","4D","5H","6S"]-->['C','D','H','S']"""
+        actual = poker_test.dokhand(["2C","3C","4D","5H","6S"])
+        expected = ['C','D','H','S']
+        self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
     unittest.main(exit=False)

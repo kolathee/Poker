c924b35e2df6e35b0a8aab9857c4901a756c6357
Kolathee <s6070002@kmitl.ac.th>
2013-10-04 15:39:49 +0700

    Add Testcase numhand

--- Test_poker.py
+++ Test_poker.py
@@ -6,7 +6,17 @@ class Testpoker(unittest.TestCase):
     def test1_numhand(self):
         """test_numhand ["2C","3C","4D","5H","6S"]-->[2,4,4,5,6]"""
         actual = poker_test.numhand(["2C","3C","4D","5H","6S"])
-        expected = [2,3,4,5,6]
+        expected = ["2","3","4","5","6"]
+        self.assertEqual(actual, expected)
+    def test2_numhand(self):
+        """test_numhand ["8D","9H","3D","3C","7S"]-->[8,9,3,3,7]"""
+        actual = poker_test.numhand(["8D","9H","3D","3C","7S"])
+        expected = ["8","9","3","3","7"]
+        self.assertEqual(actual, expected)
+    def test3_numhand(self):
+        """test_numhand ["1D","1C","1H","6S","JD"]-->[1,1,1,6,J]"""
+        actual = poker_test.numhand(["1D","1C","1H","6S","JD"])
+        expected = ["1","1","1","6","J"]
         self.assertEqual(actual, expected)
     def test1_dokhand(self):
         """test_dokhand ["2C","3C","4D","5H","6S"]-->['C','D','H','S']"""

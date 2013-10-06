62efcf5e713132b51098ab8d265c25b634804b29
Kolathee <s6070002@kmitl.ac.th>
2013-10-07 00:05:51 +0700

    ADD : " handrank " fuction
    this fuction use to return Rank on hand
    
    return (Tuple)

--- poker.py
+++ poker.py
@@ -71,3 +71,55 @@ def is_fullhouse(hand):
 	if len(numset)!=2:
 		return False
 	return (num.count(numset[0])==3 and num.count(numset[1])==2) or	(num.count(numset[0])==2 and num.count(numset[1])==3)
+
+def handrank(hand):
+	if is_royal(hand):
+		return 10
+	elif is_straightflush(hand):
+		return 9,max(numhand(hand))
+	elif is_four_of_kind(hand):
+		num=numhand(hand)
+		numset=list(set(num))
+		if num.count(numset[0])==4:
+			return 8,numset[0]
+		return 8,numset[1]
+	elif is_fullhouse(hand):
+		num=numhand(hand)
+		numset=list(set(num))
+		if num.count(numset[0])==3:
+			return 7,numset[0],numset[1]
+		return 7,numset[1],numset[0]
+	elif is_flush(hand):
+		return 6,max(numhand(hand))
+	elif is_straight(hand):
+		return 5,max(numhand(hand))
+	elif is_three_of_kind(hand):
+		num=numhand(hand)
+		numset=list(set(num))
+		for each in numset:
+			if num.count(each)==3:
+				cha=numset.pop(numset.index(each))
+				return 4,cha,max(numset),min(numset)
+	elif is_two_pair(hand):
+		num=numhand(hand)
+		numset=list(set(num))
+		liTwo=[]
+		for each in numset:
+			if num.count(each)==2:
+				liTwo.append(each)
+				liTwo.remove(each)
+		return 3,max(liTwo),min(liTwo),numset[0]
+	elif is_one_pair(hand):
+		num=numhand(hand)
+		numset=list(set(num))
+		numset.sort(reverse=True)
+		liTwo=[]
+		for each in numset:
+			if num.count(each)==2:
+				liTwo.append(each)
+				liTwo.remove(each)
+		return 2,liTwo[0],numset[0],numset[1],numset[2]
+	else:
+		num=numhand(hand)
+		num.sort(reverse=True)
+		return 1,num[0],num[1]

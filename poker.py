d8aafa25df974ba55e6727d1150e771feff25d3f
Kolathee <s6070002@kmitl.ac.th>
2013-10-09 00:14:30 +0700

    Bug FIX : Syntaxerror in " dokhand " function
    write over tab at line1 --> 2Tab
    Fix : Delete 1 Tab

--- poker.py
+++ poker.py
@@ -11,7 +11,7 @@ def numhand(hand):
 	return rank
 
 def dokhand(hand):
-	suit=['-CDHS'.index(s)/10.0 for r,s in hand]
+    suit=['-CDHS'.index(s)/10.0 for r,s in hand]
     return suit
 
 def is_flush(hand):
